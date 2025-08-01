443c929c71918dc65a696e5d16c0b68c78dde5eb
Daniel Hernandez <daniel@poliwin.mx>
2025-08-01 15:29:45 -0600

    Se agregan los endpoints de brands (CRUD).

---
+++ src/brands/brands.service.ts
@@ -0,0 +1,62 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { CreateBrandDto } from './dto/create-brand.dto';
+import { UpdateBrandDto } from './dto/update-brand.dto';
+import { Brand } from './entities/brand.entity';
+import { v4 as uuid } from 'uuid';
+
+@Injectable()
+export class BrandsService {
+
+  private brands: Brand[] = [
+    {
+      id: uuid(),
+      name: 'Brand A',
+      cratedAt: new Date().getTime(),
+    }
+  ];
+
+  create(createBrandDto: CreateBrandDto) {
+    const newBrand = {
+      id: uuid(),
+      name: createBrandDto.name,
+      cratedAt: new Date().getTime(),
+    };
+    this.brands.push(newBrand);
+    return newBrand;
+  }
+
+  findAll() {
+    return this.brands;
+  }
+
+  findOne(id: string) {
+    const brand = this.brands.find(brand => brand.id === id);
+    if (!brand) {
+      throw new NotFoundException(`Brand with id ${id} not found`);
+    }
+    return brand;
+  }
+
+  update(id: string, updateBrandDto: UpdateBrandDto) {
+    let brandDb = this.findOne(id);
+
+    this.brands = this.brands.map(brand => {
+      if (brand.id === id) {
+        brandDb = {
+          ...brand,
+          ...updateBrandDto,
+          updatedAt: new Date().getTime(),
+          id
+        };
+        return brandDb;
+      }
+      return brand;
+    });
+    return brandDb;
+  }
+
+  remove(id: string) {
+    const brand = this.findOne(id);
+    this.brands = this.brands.filter(brand => brand.id !== id);
+  }
+}
---
+++ src/brands/entities/brand.entity.ts
@@ -0,0 +1,9 @@
+export class Brand {
+
+    id: string;
+    name: string;
+
+    cratedAt: number;
+    updatedAt?: number;
+
+}
